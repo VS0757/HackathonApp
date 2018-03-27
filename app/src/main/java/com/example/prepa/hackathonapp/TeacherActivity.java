a1d256b1c50e738ccdb039117d6001b39a5795b5
VS0757 <vedsampath@gmail.com>
2018-03-27 10:45:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/prepa/hackathonapp/TeacherActivity.java
@@ -0,0 +1,37 @@
+package com.example.prepa.hackathonapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+
+public class TeacherActivity extends AppCompatActivity {
+    public String m_Text = "";
+
+    public void promptUsername(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("User Name");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher);
+
+        promptUsername();
+
+    }
+}
